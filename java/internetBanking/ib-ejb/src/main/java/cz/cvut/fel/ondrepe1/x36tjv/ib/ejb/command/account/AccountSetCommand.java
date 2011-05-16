0263c33a749842467f274f7f4edaa3c441eb3d65
Petr Ondrejicek <p.ondrejicek@gmail.com>
2011-05-16 13:57:22 +0200

    uprava account set

--- java/internetBanking/ib-ejb/src/main/java/cz/cvut/fel/ondrepe1/x36tjv/ib/ejb/command/account/AccountSetCommand.java
+++ java/internetBanking/ib-ejb/src/main/java/cz/cvut/fel/ondrepe1/x36tjv/ib/ejb/command/account/AccountSetCommand.java
@@ -3,9 +3,10 @@ package cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.command.account;
 import cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.command.CommonSetCommand;
 import cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.po.AccountPO;
 import cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.po.CurrencyPO;
+import cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.po.CustomerAccountPO;
+import cz.cvut.fel.ondrepe1.x36tjv.ib.ejb.po.CustomerPO;
 import cz.cvut.fel.ondrepe1.x36tjv.ib.iface.ejb.exception.CommonIBException;
 import cz.cvut.fel.ondrepe1.x36tjv.ib.iface.to.Account;
-import java.math.BigDecimal;
 import java.util.Random;
 import javax.persistence.EntityManager;
 
@@ -28,6 +29,7 @@ public class AccountSetCommand extends CommonSetCommand<Account> {
   protected void execute(Account acc) {
     Random rnd = new Random();
     CurrencyPO crPo = em.find(CurrencyPO.class, acc.getCurrencyCode());
+    CustomerPO cPo = em.find(CustomerPO.class, acc.getCustomerId());
     
     AccountPO accPo = new AccountPO();
     accPo.setId(10000 + (rnd.nextInt() % 1000));
@@ -36,6 +38,14 @@ public class AccountSetCommand extends CommonSetCommand<Account> {
     accPo.setCode(crPo);
     accPo.setValid("Y");
     em.persist(accPo);
+    
+    CustomerAccountPO caPo = new CustomerAccountPO();
+    caPo.setId(10000 + (rnd.nextInt() % 1000));
+    caPo.setIdAccount(accPo);
+    caPo.setIdCustomer(cPo);
+    
+    em.persist(caPo);
+    
   }
   
 }
